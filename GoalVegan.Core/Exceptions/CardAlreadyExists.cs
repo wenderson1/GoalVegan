3baead499e408f7e0ce1a01c31449c52ac847a68
Wenderson Farias <wenderson.farias20@gmail.com>
2021-07-20 00:08:13 -0300

    criado core

---
+++ GoalVegan.Core/Exceptions/CardAlreadyExists.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoalVegan.Core
+{
+    class CardAlreadyExists:Exception
+    {
+        public CardAlreadyExists():base("Card Already exists in this account")
+        {
+
+        }
+    }
+}
