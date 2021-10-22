7c1bc778d20d3fcd54b7705dd1682bfa8e100df1
Wenderson Farias <wenderson.farias10@gmail.com>
2021-10-22 07:12:56 -0300

    Terminado as validações

--- GoalVegan.Application/Validators/CreateBuyerCommandValidator.cs
+++ GoalVegan.Application/Validators/CreateBuyerCommandValidator.cs
@@ -4,11 +4,59 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace GoalVegan.Application.Validators
 {
-    public class CreateBuyerCommandValidator:AbstractValidator<CreateBuyerCommand>
+    public class CreateBuyerCommandValidator : AbstractValidator<CreateBuyerCommand>
     {
+        public CreateBuyerCommandValidator()
+        {
+            RuleFor(b => b.Email)
+                .NotEmpty()
+                .NotNull()
+                .WithMessage("Email é obrigatório");
+
+            RuleFor(b => b.Email)
+                .EmailAddress()
+                .WithMessage("Email Inválido");
+
+            RuleFor(b => b.Password)
+                .NotEmpty()
+                .NotNull()
+                .WithMessage("Senha é obrigatório");
+
+            RuleFor(b=>b.Password)
+                .Must(ValidPassword)
+                .WithMessage("Senha deve conter 8 caracteres, um número, uma letra maiúscula, um caracter especial");
+
+            RuleFor(b => b.PhoneNumber)
+              .NotEmpty()
+              .NotNull()
+              .WithMessage("Número de Telefone é obrigatório");
+
+            RuleFor(b => b.PhoneNumber)
+                .MaximumLength(11)
+                .MinimumLength(9)
+                .WithMessage("Número de telefone deve ter de 9 a 11 caracteres");
+
+            RuleFor(b => b.Document)
+                .NotEmpty()
+                .NotNull()
+                .WithMessage("CPF é obrigatório");
+
+            RuleFor(b => b.Document)
+                .MaximumLength(12)
+                .MinimumLength(11)
+                .WithMessage("CPF deve ter 12 caracteres");
+        }
+
+        public bool ValidPassword(string password)
+        {
+            var regex = new Regex(@"^.*(?=.{8,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!*@#$%^&+=]).*$");
+
+            return regex.IsMatch(password);
+        }
     }
 }
--- GoalVegan.Application/Validators/CreateProductCommandValidator.cs
+++ GoalVegan.Application/Validators/CreateProductCommandValidator.cs
@@ -12,6 +12,56 @@ namespace GoalVegan.Application.Validators
     {
         public CreateProductCommandValidator()
         {
+            RuleFor(p => p.Title)
+                .NotEmpty()
+                .NotNull()
+                .WithMessage("Título é obrigatório");
+
+            RuleFor(p => p.Title)
+                .MinimumLength(10)
+                .MaximumLength(150)
+                .WithMessage("Título deve ter entre 10 a 150 caracteres");
+
+            RuleFor(p => p.Price)
+                .NotEmpty()
+                .NotNull()
+                .WithMessage("Preço é obrigatório");
+
+            RuleFor(p => p.Price)
+                .Must(ValidPrice)
+                .WithMessage("Preço deve ser maior que 10");
+
+            RuleFor(p => p.Description)
+                .NotEmpty()
+                .NotNull()
+                .WithMessage("Descrição é obrigatório");
+
+            RuleFor(p => p.Description)
+                .MinimumLength(10)
+                .MaximumLength(250)
+                .WithMessage("Descrição deve ter entre 10 a 250 caracteres");
+
+            RuleFor(p => p.Category)
+                .NotEmpty()
+                .NotNull()
+                .WithMessage("Categoria é obrigatório");
+
+            RuleFor(p => p.Title)
+                .MinimumLength(10)
+                .MaximumLength(60)
+                .WithMessage("Categoria deve ter entre 10 a 60 caracteres");
+        }
+
+        public bool ValidPrice(double price)
+        {
+            price.ToString("0.00");
+
+            if (price > 10)
+            {
+                return true;
+            }
+
+            return false;
         }
     }
 }
